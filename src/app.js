5e2e425851a90ded507556131169e4fe6cc65fe5
Sohil Jain <sohiljain897@gmail.com>
2025-07-16 10:06:10 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use("/test", (req,res)=>{
+    res.send("Hello from the server");
+})
+
+
+app.use("/hello", (req,res)=>{
+    res.send("Hellooooooooooowwwwwoooo");
+})
+app.listen(3000,()=>{
+    console.log("Server is listening on port 3000");
+});
