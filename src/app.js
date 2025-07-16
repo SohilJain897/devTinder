286a89affb0c64ee3e388ad62ee08ea229ee9b93
Sohil Jain <sohiljain897@gmail.com>
2025-07-16 19:56:08 +0530

    route handlers

--- src/app.js
+++ src/app.js
@@ -2,22 +2,15 @@ const express = require('express');
 const app = express();
 
 
-
-app.get("/user",(req,res) => {
-   res.send( {firstname: "Sohil" , lastName:"jain" })
-})
-
-app.post("/test", (req,res)=>{
-    res.send("Hello from the server");
-})
-
-
-app.use("/hello", (req,res)=>{
-    res.send("Hellooooooooooowwwwwoooo");
-})
-
-app.use("/", (req,res)=>{
-    res.send("Hello from the ccccc");
+app.use("/user",(req,res,next)=>{
+   // res.send("hello from the florian wirtz")
+    next();
+}, (req,res,next) => {
+   // res.send("hello from the frompng")
+    next();
+},(req,res,next)=>{
+    res.send("hello from milos kerkez")
+    next();
 })
 
 app.listen(3000,()=>{
