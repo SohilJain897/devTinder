e9692675ff8a79ac5e8e27072ecb38a2c4e796ca
Sohil Jain <sohiljain897@gmail.com>
2025-07-16 23:03:36 +0530

    writing auth middleware

--- src/app.js
+++ src/app.js
@@ -2,17 +2,31 @@ const express = require('express');
 const app = express();
 
 
-app.use("/user",(req,res,next)=>{
-   // res.send("hello from the florian wirtz")
-    next();
-}, (req,res,next) => {
-   // res.send("hello from the frompng")
-    next();
-},(req,res,next)=>{
-    res.send("hello from milos kerkez")
+app.use("/admin", (req,res,next) =>{
+    const token = "xyz";
+    const authValid = token === "xyzwwww";
+    if (!authValid){
+        res.status(401).send("Unauthorized");
+    }
+    else{
+        next();
+    }
+    
+});
+
+app.use("/admin/getapp", (req,res,next)=>{
+    res.send("getaapppp");
     next();
 })
 
+app.use("/admin/getIsak", (req,res,next)=>{
+    res.send("get ISak");
+    next();
+})
+app.use("/admin/ssss", (req,res)=>{
+    res.send("getaappsssssssspp");
+
+})
 app.listen(3000,()=>{
     console.log("Server is listening on port 3000");
 });
