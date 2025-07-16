4ef8b507c5a241175f40097815d688b5f6e14f52
Sohil Jain <sohiljain897@gmail.com>
2025-07-16 17:43:57 +0530

    exploring get post api calls

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.use("/test", (req,res)=>{
+
+
+app.get("/user",(req,res) => {
+   res.send( {firstname: "Sohil" , lastName:"jain" })
+})
+
+app.post("/test", (req,res)=>{
     res.send("Hello from the server");
 })
 
@@ -9,6 +15,11 @@ app.use("/test", (req,res)=>{
 app.use("/hello", (req,res)=>{
     res.send("Hellooooooooooowwwwwoooo");
 })
+
+app.use("/", (req,res)=>{
+    res.send("Hello from the ccccc");
+})
+
 app.listen(3000,()=>{
     console.log("Server is listening on port 3000");
 });
